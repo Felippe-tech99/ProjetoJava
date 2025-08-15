9de0e031f95e56cddd65d7749cd0af3f0b11dc51
Felippe-tech99 <fe.passini@gmail.com>
2025-08-15 18:41:06 -0300

    Primeiro commit do projeto Java

---
+++ untitled/src/Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Bem vindo ao Sreen Match !");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int ano = 2022;
+        System.out.println("Ano de Lançamento:"+ ano );
+        boolean incluidoNoPlano = true;
+        double classificacao = 8.1;
+
+        double mediaAvalicacao =  (8.1 + 6.3 + 8.0) /3;
+        System.out.println(mediaAvalicacao);
+        String sinopse;
+        sinopse = "Filme de ação dos anos 80";
+        System.out.println(sinopse);
+    }
+}
